ebebf9e599e1ed6762f524662137fac835092ceb
rehanshekh <rehanshekh123@gmail.com>
2024-08-17 20:58:01 +0530

    first

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
                 <div className="text-content">
                     <h1>SALES</h1>
                     <p>
-                       Discover exciting new ways to improve your sex life with the largest selection of adult sex toys in the US. At Lovehoney you can browse the very best online sex toys and shop with confidence, knowing that every order comes with discreet packaging. Check out our Beginner's Guide to Sex Toys to find the toy that's right for you.
+                       Discover exciting new ways to improve your sex life with the largest selection of adult sex toys in the India. At Lovehoney you can browse the very best online sex toys and shop with confidence, knowing that every order comes with discreet packaging. Check out our Beginner's Guide to Sex Toys to find the toy that's right for you.
                     </p>
                     <div className="ctas">
                         <div className="banner-cta">Read More</div>
